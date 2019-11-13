83fc442818cf0a3a50f65f34a31d39e02b2a8641
runzhe <runzhe2000@163.com>
2019-11-13 20:04:48 +0800

    change the file structure

---
+++ CPU_code/if_id.v
@@ -0,0 +1,23 @@
+module if_id(
+	input	wire					clk,
+	input 	wire 					rst,
+	// from IF
+	input 	wire[`InstAddrBus] 		if_pc,
+	input	wire[`InstBus]			if_inst,
+	// to ID
+	output	reg[`InstAddrBus]		id_pc,
+	output	reg[`InstBus]			id_inst
+);
+
+always @(posedge clk) begin
+	if (rst == `RstEnable) begin
+		id_pc <= `ZeroWord;		
+		id_inst <= `ZeroWord;
+	end
+	else begin
+		id_pc <= if_pc;
+		id_inst <= if_inst;
+	end
+end
+
+endmodule
