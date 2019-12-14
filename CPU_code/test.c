54b282eebdc64c841b93f228f8aeff93867472ee
runzhe <runzhe2000@163.com>
2019-12-14 23:24:44 +0800

    simpleloop takes 26115 ns

--- CPU_code/test.c
+++ CPU_code/test.c
@@ -1,29 +1,24 @@
 #include "io.h"
-//input: 1 2 3 4
 
-int a[4];
-int main()
-{
-    int b[4];
-    b[0] = 1;
-    b[1] = 2;
-    b[2] = 3;
-    b[3] = 4;
-	int i;
-    for (i = 0; i < 4; i++)
-	{
-		a[i] = 0;
-		// b[i] = inl();
-	}
-	for (i = 0; i < 4; i++)
-	{
-		outl(a[i]);
-	}
-	println("");
-	int *p;
-	p=b;
-	for (i = 0; i < 4; i++)
-	{
-		outl(p[i]);
-	}
+int N;
+int h = 99;
+int i = 100;
+int j = 101;
+int k = 102;
+int total = 0;
+
+int main() {
+  	int a;
+    int b;
+	int c;
+	// N=inl();
+	N = 20;
+	for ( a=1; a<=N; a++ )
+	for ( b=1; b<=N; b++ )
+	for ( c=1; c<=N; c++ )
+		total += a * h + b * i + c * j;
+	
+	outlln(total);
+	// printf("%d\n",total);
+	return 0;
 }
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-CPU
+What can I do?
+
+1. dynamic prediction
+2. implement JAL and JALR in IF stage
+3. data cache
